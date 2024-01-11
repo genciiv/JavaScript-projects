d6c256c9c57a42259650ff2ae4f003e566eabf08
genciiv <vaqogenci@gmail.com>
2024-01-11 09:42:47 +0100

    fixed

--- to do list/script.js
+++ to do list/script.js
@@ -23,27 +23,3 @@ function createTask(taskText) {
   task.textContent = taskText;
   taskList.appendChild(task);
 }
-
-const bookingForm = document.getElementById("bookingForm");
-
-bookingForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const name = document.getElementById("name").value.trim();
-  const checkInDate = document.getElementById("checkInDate").value;
-  const checkOutDate = document.getElementById("checkOutDate").value;
-  const roomType = document.getElementById("roomType").value;
-
-  const bookingDetails = {
-    name,
-    checkInDate,
-    checkOutDate,
-    roomType,
-  };
-
-  // Ruaj informacionin e rezervimit në localStorage
-  localStorage.setItem("bookingDetails", JSON.stringify(bookingDetails));
-
-  // Përcjell përdoruesin në faqen e detajeve të rezervimit
-  window.location.href = "bookingDetails.html";
-});
